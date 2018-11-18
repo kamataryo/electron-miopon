093468be42597952268bc4d5106246587760e5c2
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2018-11-18 11:36:43 +0900

    Coupon remaining formatter

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const Store = require('./lib/store')
 const { getAccessToken, setAccessToken } = require('./lib/keychain')
 const oAuth = require('./lib/o-auth')
+const axios = require('axios')
+const { formatCouponInfo } = require('./lib/format')
 const store = new Store({ configName: 'user-data' })
 const EXPIRES_AT = 'expires_at'
-
-// NOTE: 初回認証時にプロセスが落ちてしまう
 
 Promise.all([getAccessToken(), store.get(EXPIRES_AT)])
   .catch(err => console.log(err.type))
@@ -16,4 +16,28 @@ Promise.all([getAccessToken(), store.get(EXPIRES_AT)])
   .then(({ accessToken, expiresAt }) => {
     store.set(EXPIRES_AT, expiresAt)
     return setAccessToken(accessToken)
+  })
+  .then(({ accessToken, developerId }) => {
+    axios.defaults.headers.common['X-IIJmio-Developer'] = developerId
+    axios.defaults.headers.common['X-IIJmio-Authorization'] = accessToken
+    axios.defaults.headers.put['Content-Type'] = 'appication/jsonn'
+
+    // setInterval(() => {
+    // クーポン残量照会・クーポンのON/OFF状態照会 5 requests/min.
+    axios
+      .get('https://api.iijmio.jp/mobile/d/v2/coupon/')
+      .then(
+        res =>
+          res.data.returnCode === 'OK' &&
+          console.log(formatCouponInfo(res.data.couponInfo))
+      )
+    // }, 30 * 1000)
+
+    // // クーポンのON/OFF 1 request / min.
+    // axios.put('https://api.iijmio.jp/mobile/d/v2/coupon/').then(console.log)
+
+    // データ利用量照会 5 requests / min.
+    // axios
+    //   .get('https://api.iijmio.jp/mobile/d/v2/log/packet/')
+    //   .then(res => console.log(res.data))
   })
